22300a75af728e35903be22a85d89820782a3ba7
ilSPRITEli <gvkgodvilekung@gmail.com>
2023-09-12 22:59:54 +0700

    Update subscript.php

--- backend/subscript.php
+++ backend/subscript.php
@@ -1,53 +1,52 @@
 <?php
-include 'database.php';
-session_start();
-$email = $_POST['email'];
-$password = $_POST['password'];
-$plan_id = $_POST['plan'];
+    include 'database.php';
+    session_start();
+    $email = $_POST['email'];
+    $password = $_POST['password'];
+    $plan_id = $_POST['plan'];
 
-$sql = "SELECT * FROM register WHERE email = '{$email}' AND password = '{$password}'";
-$result = $conn->query($sql);
+    $sql = "SELECT * FROM register WHERE email = '{$email}' AND password = '{$password}'";
+    $result = $conn->query($sql);
 
-if ($result->num_rows > 0) {
+    if ($result->num_rows > 0) {
 
-  while ($row = $result->fetch_assoc()) {
-    $_SESSION['id'] = $row['id'];
-    $_SESSION['name'] = $row['name'];
-    $_SESSION['password'] = $row['password'];
-    $_SESSION['gender'] = $row['gender'];
-  }
-
-
-  $subscription_sql = "SELECT * FROM subscription_plans WHERE id = {$plan_id}";
-  $subscription_result = $conn->query($subscription_sql);
-
-  if ($subscription_result->num_rows > 0) {
-    $subscription_row = $subscription_result->fetch_assoc();
-    $plan_name = $subscription_row['name'];
-    $plan_price = $subscription_row['price'];
-    $plan_duration = $subscription_row['duration'];
+      while ($row = $result->fetch_assoc()) {
+        $_SESSION['id'] = $row['id'];
+        $_SESSION['name'] = $row['name'];
+        $_SESSION['password'] = $row['password'];
+      }
 
 
-    $current_date = date('Y-m-d');
-    $start_date = $current_date;
-    $end_date = date('Y-m-d', strtotime("+$plan_duration days", strtotime($current_date)));
+      $subscription_sql = "SELECT * FROM subscription_plans WHERE id = {$plan_id}";
+      $subscription_result = $conn->query($subscription_sql);
+
+      if ($subscription_result->num_rows > 0) {
+        $subscription_row = $subscription_result->fetch_assoc();
+        $plan_name = $subscription_row['name'];
+        $plan_price = $subscription_row['price'];
+        $plan_duration = $subscription_row['duration'];
 
 
-    $user_id = $_SESSION['id'];
-    $insert_subscription_sql = "INSERT INTO user_subscriptions (user_id, plan_id, start_date, end_date) VALUES ({$user_id}, {$plan_id}, '{$start_date}', '{$end_date}')";
+        $current_date = date('Y-m-d');
+        $start_date = $current_date;
+        $end_date = date('Y-m-d', strtotime("+$plan_duration days", strtotime($current_date)));
 
-    if ($conn->query($insert_subscription_sql) === TRUE) {
 
-      $_SESSION['subscription_status'] = "You have subscribed to the '{$plan_name}' plan for {$plan_duration} days.";
+        $user_id = $_SESSION['id'];
+        $insert_subscription_sql = "INSERT INTO user_subscriptions (user_id, plan_id, date_start, date_end) VALUES ({$user_id}, {$plan_id}, '{$start_date}', '{$end_date}')";
+
+        if ($conn->query($insert_subscription_sql) === TRUE) {
+
+          $_SESSION['subscription_status'] = "You have subscribed to the '{$plan_name}' plan for {$plan_duration} days.";
+        } else {
+          $_SESSION['subscription_status'] = "Subscription failed: " . $conn->error;
+        }
+      } else {
+        $_SESSION['subscription_status'] = "Invalid subscription plan selected.";
+      }
     } else {
-      $_SESSION['subscription_status'] = "Subscription failed: " . $conn->error;
+      echo "0 results";
     }
-  } else {
-    $_SESSION['subscription_status'] = "Invalid subscription plan selected.";
-  }
-} else {
-  echo "0 results";
-}
 
-header('location:../frontend/index.php');
+    header('location:../frontend/index.php');
 ?>

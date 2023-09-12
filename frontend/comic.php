6c88bf50ad7df2da4d83676e5605d44a994fa8a6
bamibibi <Natthacha9876@gmail.com>
2023-09-12 19:06:31 +0700

    bamibibi commit

---
+++ frontend/comic.php
@@ -0,0 +1,56 @@
+<!DOCTYPE html>
+    <head>
+        <title>HONGSAMUT</title>
+        <link rel="stylesheet" href="css/comic.css">
+        <link rel="stylesheet" href="plugins/bootstrap/css/bootstrap.min.css">
+        <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.10.5/font/bootstrap-icons.css">
+        
+    </head>
+    <body>
+        <header class="header"> 
+            <H2 class="logo"><a href="home-newarrival.html">HONGSAMUT</a></H2>
+            <nav class="menubar">
+
+                <a href="all.html">VIEW BOOK</a>
+                <a href="backpack.html">BACKPACK</a>
+
+                <a href="#">bibi</a>
+                <i class="bi bi-person-circle"></i>
+                
+                
+            </nav>
+        </header>
+        
+        <div class="main">
+            <div class="leftbar"> 
+                <h3><a href="home-newarrival.html">NEW ARRIVALS</a></h3>
+                <header>CATEGORIES</header>
+                <div class="leftbarlist">
+                    <a href="all.html" class="all">ALL BOOKS</a>
+                    <a href="comic.html" class="comicleft">COMIC</a>
+                    <a href="#">DETECTIVE</a>
+                    <a href="#">FANTASY</a>
+                    <a href="#">FICTION</a>
+                    <a href="#">GUIDE</a>
+                    <a href="#">HEALTH</a>
+                    <a href="#">HISTORY</a>
+                    <a href="#">HORROR</a>
+                    <a href="#">KNOWLEDGE</a>
+                    <a href="#">MYSTERY</a>
+                    <a href="#">ROMANCE</a>
+                </div>  
+
+            </div>
+            <div class="mid">
+                <div class="comic ">Comic</div>
+                <div class="container">
+                    <div class="nabox">Book1</div>
+                    <div class="nabox">Book2</div>
+                    <div class="nabox">Book3</div>
+                    <div class="nabox">Book4</div>
+                    <div class="nabox">Book5</div>
+            </div>
+            
+        </div>
+    </body>
+</html>

5d0cc40693550d663b970d889fd8b79fc0c75107
MyNNeiei <kullananzaza@gmail.com>
2023-09-14 02:48:48 +0700

    nncommit2

--- frontend/login.php
+++ frontend/login.php
@@ -8,11 +8,12 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.2/css/all.min.css" integrity="sha512-z3gLpd7yknf1YoNbCzqRKc4qyor8gaKU1qmn+CShxbuBusANI9QpRohGBreCFkKxLhei6S9CQXFEbbKuqLg0DA==" crossorigin="anonymous" referrerpolicy="no-referrer" />
     <link rel="stylesheet" href="./css/login.css">
 </head>
 <body>
-<div class="nav-bar">
-            <h1><a href="../frontend/index.php">Home</a></h1>
+<div class="back">
+            <h2><a href="../frontend/index.php"><i class="fa fa-backward" aria-hidden="true"></i>Back</a></h2>
 </div>
     <div class="main">
         <h1>Login</h1>

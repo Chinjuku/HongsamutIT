ff3851647754ef07c5ff719d8c0a7258b68a274c
Your Name <you@example.com>
2023-09-14 01:29:00 +0700

    edit addbook not finish

--- backend/addbook.php
+++ backend/addbook.php
@@ -3,14 +3,29 @@
 
     $book_name = $_POST['bookname'];
     $book_owner = $_POST['bookowner'];
-    $status = $_POST['status'];
+    $cate_id = $_POST['cate_id']; //รับมาจากไอดีหมวดหมู่ 1-7 (เบ็ดเตล็ด == 8 - ค่าเริ่มต้น)
+    $status = $_POST['status']; //0 false , 1 true
 
-    $sql = "SELECT book_name, book_owner FROM books WHERE book_name = null or book_owner = null";
+    $sql = "SELECT book_name, book_owner FROM books WHERE book_name = null LIMIT 0";
+    $result1 = $conn->query($sql);
 
+    $sql2 = "SELECT book_name, book_owner FROM books WHERE book_owner = null LIMIT 0";
+    $result2 = $conn->query($sql2);
 
-    $sql = "INSERT INTO books (name, email, password, gender)
-    VALUES ('{$book_name}', '{$book_owner}', '{$password}', '{$gender}')";
-
-
+    if($result1->num_rows == 0){
+        echo '<script>alert(You need to type your book name!)</script>';
+        header('../frontend/addbook.php');
+    }
+    else if($result2->num_rows == 0){
+        echo '<script>alert(You need to type your owner book!)</script>';
+        header('../frontend/addbook.php');
+    }
+    else{
+        // if(ชื่อหนังสือกับชื่อเจ้าของตรงกัน){
+            //เพิ่มจำนวนหนังสือ +1
+        // }
+        $sql = "INSERT INTO books (book_name, book_owner, cate_id, status)
+        VALUES ('{$book_name}', '{$book_owner}', '{$cate_id}', '{$status}')";
+    }
 
 ?>

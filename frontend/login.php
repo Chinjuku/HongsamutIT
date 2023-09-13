09ebc96e9086efb08fd62ed5f198c9f7625e03b0
MyNNeiei <kullananzaza@gmail.com>
2023-09-14 00:40:22 +0700

    nncommit1

--- frontend/login.php
+++ frontend/login.php
@@ -1,7 +1,7 @@
-<?php
+<!-- <?php
     // include "layout/navbar.php";
     // include 'layout/page.php';
-?>
+?> -->
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -11,11 +11,24 @@
     <link rel="stylesheet" href="./css/login.css">
 </head>
 <body>
-    <a href="../frontend/index.php">Back</a>
-    <form action="../backend/login.php" method="post">
-        <input type="text" name="user_name" id="username">
-        <input type="password" name="password" id="password">
-        <br><button type="submit">sign in</button>
-    </form>
+<div class="nav-bar">
+            <h1><a href="../frontend/index.php">Home</a></h1>
+</div>
+    <div class="main">
+        <h1>Login</h1>
+        <form action="../backend/login.php" method="post">
+            <div class="txt_field">
+                <input type="text" name="user_name" id="username">
+                <span></span><label>Username</label></div>
+            <div class="txt_field">
+                <input type="password" name="password" id="password">
+                <span></span><label>Password</label>
+            </div>
+            <button type="submit">Login</button>
+            <div class="signup_link">
+                Not member? <a href="#">Sign up</a>
+            </div>
+        </form>
+</div>
 </body>
 </html>

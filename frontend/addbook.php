eb2cd643a156b63c749bdada12ee26b65345970f
cheraxm <109953279+cheraxm@users.noreply.github.com>
2023-09-14 23:07:53 +0700

    create addbook.php

--- frontend/addbook.php
+++ frontend/addbook.php
@@ -1,3 +1,29 @@
 <?php
     include 'layout/navbar.php';
 ?>
+
+<!Document html>
+<html lang="en">
+<head>
+    <title>HONGSAMUT</title>
+    <link rel="stylesheet" href="css/addbook.css">
+    <link rel="stylesheet" href="plugins/bootstrap/css/bootstrap.min.css">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.10.5/font/bootstrap-icons.css">
+</head>
+<body>
+    <section class="add-books">
+        <h1 class="title">Book</h1>
+
+        <form action="" method="post" enctype="multipart/form-data">
+            <h3>add a books</h3>
+            <input type="text" name="name" class="box" placeholder="enter book name" require>
+            <input type="text" name="name" class="box" placeholder="enter author" require>
+            <input type="text" name="name" class="box" placeholder="enter category" require>
+            <input type="file" accept="image/jpg, image/jpeg, image/png" class="box" require>
+            <input type="submit" value="add book" name="add_book" class="btn">
+        </form>
+
+    </section>
+    
+</body>
+</html>

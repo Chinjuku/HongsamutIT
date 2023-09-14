253d3f769e0d5c31a0c257d61fea5352bdeb1f1f
Your Name <you@example.com>
2023-09-14 17:55:06 +0700

    ;

--- backend/login.php
+++ backend/login.php
@@ -21,7 +21,7 @@ if ($result->num_rows > 0) {
     $_SESSION['password'] = $row['password'];
 
   }
-  echo '<script>window.location.href = "../frontend/index.php";</script>'
+  echo '<script>window.location.href = "../frontend/index.php";</script>';
   
 } else {
   // echo '<script>alert("Your account is incorrect.");</script>';

61914c5672023b51127500e9866816fcc15a372b
ilSPRITEli <gvkgodvilekung@gmail.com>
2023-09-16 16:47:10 +0700

    landing

--- index.php
+++ index.php
@@ -1,1 +1,31 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <link rel="stylesheet" href="globals.css" />
+    <link rel="stylesheet" href="style.css" />
+  </head>
+  <body>
+    <div class="LANDING">
+      <div class="div">
+        <div class="text-wrapper">EXPLORE OUR BOOKS</div>
+        <div class="text-wrapper-2">or</div>
+        <div class="text-wrapper-3">dont’t have and account?</div>
+        <div class="text-wrapper-4">REGISTER</div>
+        <img class="vector" src="img/image.svg" />
+        <img class="img" src="img/vector-3.svg" />
+        <img class="vector-2" src="img/vector.svg" />
+        <div class="overlap">
+          <img class="vector-3" src="img/vector-2.svg" />
+          <div class="text-wrapper-5">ห้องสมุดสุดเฟี้ยว</div>
+        </div>
+        <img class="line" src="img/line-1.svg" />
+        <img class="line-2" src="img/line-2.svg" />
+        <button class="login-button">
+          <div class="overlap-group"><div class="text-wrapper-6">LOGIN</div></div>
+        </button>
+      </div>
+    </div>
+  </body>
+</html>
+
 echo '<script>window.location.href = "../frontend/index.php";</script>'

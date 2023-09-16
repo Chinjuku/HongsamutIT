88d6e233b6315b9a4d263c4d06e67c839931d81b
ilSPRITEli <gvkgodvilekung@gmail.com>
2023-09-16 19:16:50 +0700

    rftfhg

--- index.php
+++ index.php
@@ -1,152 +1,110 @@
 <!DOCTYPE html>
 <html lang="english">
+  <head>
+    <title>exported project</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <meta charset="utf-8" />
+    <meta property="twitter:card" content="summary_large_image" />
 
-<head>
-  <title>HONGSAMUT IT</title>
-  <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-  <meta charset="utf-8" />
-  <meta property="twitter:card" content="summary_large_image" />
+    <style data-tag="reset-style-sheet">
+      html {  line-height: 1.15;}body {  margin: 0;}* {  box-sizing: border-box;  border-width: 0;  border-style: solid;}p,li,ul,pre,div,h1,h2,h3,h4,h5,h6,figure,blockquote,figcaption {  margin: 0;  padding: 0;}button {  background-color: transparent;}button,input,optgroup,select,textarea {  font-family: inherit;  font-size: 100%;  line-height: 1.15;  margin: 0;}button,select {  text-transform: none;}button,[type="button"],[type="reset"],[type="submit"] {  -webkit-appearance: button;}button::-moz-focus-inner,[type="button"]::-moz-focus-inner,[type="reset"]::-moz-focus-inner,[type="submit"]::-moz-focus-inner {  border-style: none;  padding: 0;}button:-moz-focus,[type="button"]:-moz-focus,[type="reset"]:-moz-focus,[type="submit"]:-moz-focus {  outline: 1px dotted ButtonText;}a {  color: inherit;  text-decoration: inherit;}input {  padding: 2px 4px;}img {  display: block;}html { scroll-behavior: smooth  }
+    </style>
+    <style data-tag="default-style-sheet">
+      html {
+        font-family: Inter;
+        font-size: 16px;
+      }
 
-  <style data-tag="reset-style-sheet">
-    html {
-      line-height: 1.15;
-    }
+      body {
+        font-weight: 400;
+        font-style:normal;
+        text-decoration: none;
+        text-transform: none;
+        letter-spacing: normal;
+        line-height: 1.15;
+        color: var(--dl-color-gray-black);
+        background-color: var(--dl-color-gray-white);
 
-    body {
-      margin: 0;
-    }
+      }
+    </style>
+    <link
+      rel="stylesheet"
+      href="https://fonts.googleapis.com/css2?family=Inter:wght@100;200;300;400;500;600;700;800;900&amp;display=swap"
+      data-tag="font"
+    />
+    <link
+      rel="stylesheet"
+      href="https://fonts.googleapis.com/css2?family=Inter:wght@100;200;300;400;500;600;700;800;900&amp;display=swap"
+      data-tag="font"
+    />
+    <link
+      rel="stylesheet"
+      href="https://fonts.googleapis.com/css2?family=Inter:wght@100;200;300;400;500;600;700;800;900&amp;display=swap"
+      data-tag="font"
+    />
+    <link
+      rel="stylesheet"
+      href="https://fonts.googleapis.com/css2?family=Mitr:wght@200;300;400;500;600;700&amp;display=swap"
+      data-tag="font"
+    />
+    <link
+      rel="stylesheet"
+      href="https://fonts.googleapis.com/css2?family=Mitr:wght@200;300;400;500;600;700&amp;display=swap"
+      data-tag="font"
+    />
+  </head>
+  <body>
+    <link rel="stylesheet" href="./style.css" />
+    <div>
+      <link href="./index.css" rel="stylesheet" />
 
-    * {
-      box-sizing: border-box;
-      border-width: 0;
-      border-style: solid;
-    }
-
-    p, li, ul, pre, div, h1, h2, h3, h4, h5, h6, figure, blockquote, figcaption {
-      margin: 0;
-      padding: 0;
-    }
-
-    button {
-      background-color: transparent;
-    }
-
-    button, input, optgroup, select, textarea {
-      font-family: inherit;
-      font-size: 100%;
-      line-height: 1.15;
-      margin: 0;
-    }
-
-    button, select {
-      text-transform: none;
-    }
-
-    button,
-    [type="button"],
-    [type="reset"],
-    [type="submit"] {
-      -webkit-appearance: button;
-    }
-
-    button::-moz-focus-inner,
-    [type="button"]::-moz-focus-inner,
-    [type="reset"]::-moz-focus-inner,
-    [type="submit"]::-moz-focus-inner {
-      border-style: none;
-      padding: 0;
-    }
-
-    button:-moz-focus,
-    [type="button"]:-moz-focus,
-    [type="reset"]:-moz-focus,
-    [type="submit"]:-moz-focus {
-      outline: 1px dotted ButtonText;
-    }
-
-    a {
-      color: inherit;
-      text-decoration: inherit;
-    }
-
-    input {
-      padding: 2px 4px;
-    }
-
-    img {
-      display: block;
-    }
-
-    html {
-      scroll-behavior: smooth
-    }
-  </style>
-  
-  <style data-tag="default-style-sheet">
-    html {
-      font-family: Inter;
-      font-size: 16px;
-    }
-
-    body {
-      font-weight: 400;
-      font-style: normal;
-      text-decoration: none;
-      text-transform: none;
-      letter-spacing: normal;
-      line-height: 1.15;
-      color: var(--dl-color-gray-black);
-      background-color: var(--dl-color-gray-white);
-
-    }
-  </style>
-  <link rel="stylesheet"
-    href="https://fonts.googleapis.com/css2?family=Inter:wght@100;200;300;400;500;600;700;800;900&amp;display=swap"
-    data-tag="font" />
-  <link rel="stylesheet"
-    href="https://fonts.googleapis.com/css2?family=Inter:wght@100;200;300;400;500;600;700;800;900&amp;display=swap"
-    data-tag="font" />
-  <link rel="stylesheet"
-    href="https://fonts.googleapis.com/css2?family=Inter:wght@100;200;300;400;500;600;700;800;900&amp;display=swap"
-    data-tag="font" />
-  <link rel="stylesheet"
-    href="https://fonts.googleapis.com/css2?family=Mitr:wght@200;300;400;500;600;700&amp;display=swap"
-    data-tag="font" />
-  <link rel="stylesheet"
-    href="https://fonts.googleapis.com/css2?family=Mitr:wght@200;300;400;500;600;700&amp;display=swap"
-    data-tag="font" />
-</head>
-
-<body>
-  <link rel="stylesheet" href="./style.css" />
-  <div>
-    <link href="./index.css" rel="stylesheet" />
-
-    <div class="landing-container">
-      <div class="landing-landing">
-        <span class="landing-text button">
-          <span>EXPLORE OUR BOOKS</span>
-        </span>
-        <span class="landing-text02"><span>or</span></span>
-        <div class="landing-container1">
-          <span class="landing-text04">
-            <span>dont’t have and account?</span>
+      <div class="landing-container">
+        <div class="landing-landing">
+          <span class="landing-text button">
+            <span>EXPLORE OUR BOOKS</span>
           </span>
-          <span class="landing-text06 button">REGISTER</span>
-        </div>
-        <img src="public/external/leftlsvg3512-03rk.svg" alt="leftlsvg3512" class="landing-leftlsvg" />
-        <img src="public/external/rightlsvg4544-4coe.svg" alt="rightlsvg4544" class="landing-rightlsvg" />
-        <img src="public/external/bookssvg4542-c5l8.svg" alt="bookssvg4542" class="landing-bookssvg" />
-        <img src="public/external/logosvg4543-bevd.svg" alt="logosvg4543" class="landing-logosvg" />
-        <span class="landing-text07"><span>ห้องสมุดสุดเฟี้ยว</span></span>
-        <img src="public/external/lineleft3519-k7a8.svg" alt="lineleft3519" class="landing-lineleft" />
-        <img src="public/external/lineright3519-kswb.svg" alt="lineright3519" class="landing-lineright" />
-        <div class="landing-frame14button button">
-          <span class="landing-text09"><span>LOGIN</span></span>
+          <span class="landing-text02 button">
+            <span class="landing-text03">doesn't have account?</span>
+            <span class="landing-text04"></span>
+            <span class="landing-text05">REGISTER</span>
+          </span>
+          <span class="landing-text06"><span>or</span></span>
+          <img
+            src="public/external/leftlsvg3512-03rk.svg"
+            alt="leftlsvg3512"
+            class="landing-leftlsvg"
+          />
+          <img
+            src="public/external/rightlsvg4544-4coe.svg"
+            alt="rightlsvg4544"
+            class="landing-rightlsvg"
+          />
+          <img
+            src="public/external/bookssvg4542-c5l8.svg"
+            alt="bookssvg4542"
+            class="landing-bookssvg"
+          />
+          <img
+            src="public/external/logosvg4543-bevd.svg"
+            alt="logosvg4543"
+            class="landing-logosvg"
+          />
+          <span class="landing-text08"><span>ห้องสมุดสุดเฟี้ยว</span></span>
+          <img
+            src="public/external/lineleft3519-k7a8.svg"
+            alt="lineleft3519"
+            class="landing-lineleft"
+          />
+          <img
+            src="public/external/lineright3519-kswb.svg"
+            alt="lineright3519"
+            class="landing-lineright"
+          />
+          <div class="landing-frame14button button">
+            <span class="landing-text10"><span>LOGIN</span></span>
+          </div>
         </div>
       </div>
     </div>
-  </div>
-</body>
- 
+  </body>
 </html>

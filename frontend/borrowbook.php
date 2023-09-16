c09d13c5ad430126f6ffc2c1d7b54bca58226370
MyNNeiei <kullananzaza@gmail.com>
2023-09-16 19:38:49 +0700

    nncommit11

--- frontend/borrowbook.php
+++ frontend/borrowbook.php
@@ -12,7 +12,7 @@
         
         <link rel="stylesheet" href="plugins/bootstrap/css/bootstrap.min.css">
         <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.10.5/font/bootstrap-icons.css">
-
+</head>
 
         <div class="popup" id="popup-1">
             <div class="overlay"></div>
@@ -40,7 +40,7 @@
             <button onclick="togglePopup()"> CLICK TO VIEW</button>
         </div>
     </head>
-    <script src="javascript/borrowingbook.js"></script>
+    <script src="javascript/borrowbook.js"></script>
     <body>
 
 </html>

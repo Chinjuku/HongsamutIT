ac1a53b3f91a25e44b9e475fb93aadbc995ae196
ilSPRITEli <gvkgodvilekung@gmail.com>
2023-09-16 18:17:27 +0700

    asdas

--- index.php
+++ index.php
@@ -11,44 +11,48 @@
 <div>
   <div class="landing-container">
     <div class="landing-landing">
-      <span class="landing-text"><span>EXPLORE OUR BOOKS</span></span>
-      <span class="landing-text2">or</span>
+      <span class="landing-text button"><span>EXPLORE OUR BOOKS</span></span>
+      <span class="landing-text02"><span>or</span></span>
       <div class="landing-container1">
-        <span class="landing-text3"><span>dont’t have and account?</span></span>
-        <span class="landing-text5"><span>REGISTER</span></span>
+        <span class="landing-text04">
+          <span>don't have and account?</span>
+        </span>
+        <span class="landing-text06 button">REGISTER</span>
       </div>
       <img
-        src="/leftlsvg3512-nic4.svg"
+        src="/leftlsvg3512-03rk.svg"
         alt="leftlsvg3512"
         class="landing-leftlsvg"
       />
       <img
-        src="/rightlsvg4544-b3ck.svg"
+        src="/rightlsvg4544-4coe.svg"
         alt="rightlsvg4544"
         class="landing-rightlsvg"
       />
       <img
-        src="/bookssvg4542-j9y.svg"
+        src="/bookssvg4542-c5l8.svg"
         alt="bookssvg4542"
         class="landing-bookssvg"
       />
       <img
-        src="/logosvg4543-60bn.svg"
+        src="/logosvg4543-bevd.svg"
         alt="logosvg4543"
         class="landing-logosvg"
       />
-      <span class="landing-text7"><span>ห้องสมุดสุดเฟี้ยว</span></span>
+      <span class="landing-text07"><span>ห้องสมุดสุดเฟี้ยว</span></span>
       <img
-        src="/lineleft3519-85n5.svg"
+        src="/lineleft3519-k7a8.svg"
         alt="lineleft3519"
         class="landing-lineleft"
       />
       <img
-        src="/lineright3519-ic6e.svg"
+        src="/lineright3519-kswb.svg"
         alt="lineright3519"
         class="landing-lineright"
       />
-      <button class="landing-loginbuttoninput button" type="submit">LOGIN</button>
+      <div class="landing-frame14button button">
+        <span class="landing-text09"><span>LOGIN</span></span>
+      </div>
     </div>
   </div>
 </div>

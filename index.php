58e049304156f6a12867a681688767dea2bf3dcc
ilSPRITEli <gvkgodvilekung@gmail.com>
2023-09-16 19:49:41 +0700

    Update index.php

--- index.php
+++ index.php
@@ -39,7 +39,7 @@
     />
     <link
       rel="stylesheet"
-      href="https://fonts.googleapis.com/css2?family=Inter:wght@100;200;300;400;500;600;700;800;900&amp;display=swap"
+      href="https://fonts.googleapis.com/css2?family=Inter:wght@100;200;300;400;500;600;700;800;900&amp;display=swap" 
       data-tag="font"
     />
     <link

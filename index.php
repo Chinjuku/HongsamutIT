512e5d6fc4a424ae7437309066b179c8b4290e94
ilSPRITEli <gvkgodvilekung@gmail.com>
2023-09-16 17:33:12 +0700

    Update index.php

--- index.php
+++ index.php
@@ -1,5 +1,13 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=1920, height=1080, initial-scale=1">
+    <title>Document</title>
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.2/css/all.min.css" integrity="sha512-z3gLpd7yknf1YoNbCzqRKc4qyor8gaKU1qmn+CShxbuBusANI9QpRohGBreCFkKxLhei6S9CQXFEbbKuqLg0DA==" crossorigin="anonymous" referrerpolicy="no-referrer" />
+    <link href="/landing.css" rel="stylesheet" />
+</head>
 <div>
-  <link href="./landing.css" rel="stylesheet" />
   <div class="landing-container">
     <div class="landing-landing">
       <span class="landing-text"><span>EXPLORE OUR BOOKS</span></span>

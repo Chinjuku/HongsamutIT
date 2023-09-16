606b8c42308016573feaa78ecd1a7077ad8576c6
ilSPRITEli <gvkgodvilekung@gmail.com>
2023-09-16 18:10:49 +0700

    Update index.php

--- index.php
+++ index.php
@@ -5,7 +5,7 @@
     <meta name="viewport" content="width=1920, height=1080, initial-scale=1">
     <title>Document</title>
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.2/css/all.min.css" integrity="sha512-z3gLpd7yknf1YoNbCzqRKc4qyor8gaKU1qmn+CShxbuBusANI9QpRohGBreCFkKxLhei6S9CQXFEbbKuqLg0DA==" crossorigin="anonymous" referrerpolicy="no-referrer" />
-    <link href="/landing.css" rel="stylesheet" />
+    <link href="./landing.css" rel="stylesheet" />
 </head>
 <body>
 <div>

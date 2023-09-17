693f3c3a51ad8b07dc188e0873c676f5fc000338
bamibibi <Natthacha9876@gmail.com>
2023-09-17 22:25:26 +0700

    bibi

--- frontend/all.php
+++ frontend/all.php
@@ -1,5 +1,5 @@
 <?php
-session_start();
+    // session_start();
     include './layout/navbar.php';
     include './layout/sidebar.php';
     
@@ -8,26 +8,51 @@ session_start();
 <!DOCTYPE html>
     <head>
         <title>HONGSAMUT</title>
-        <meta charset="UTF-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1.0">
-        <link rel="stylesheet" href="./css/all.css">
+        <meta name="viewport" content="width=1920, height=1080, initial-scale=1">
+        <link rel="stylesheet" href="css/viewbook.css">
         <link rel="stylesheet" href="plugins/bootstrap/css/bootstrap.min.css">
         <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.10.5/font/bootstrap-icons.css">
-        
+        <style>
+            img{
+                width: 150px;
+                height: 180px;
+            }
+        </style>
     </head>
     <body>
 
-            </div>
+        <div class="main">
             <div class="mid">
-                <div class="allbooks ">ALL BOOKS</div>
+                <div class="head">
+                    <div class="square"></div>
+                    <div class="newarrival"><b>NEW</b><br>ARRIVALS</div>
+                    
+                </div>
+                
                 <div class="container">
-                    <div class="nabox">Box1</div>
-                    <div class="nabox">Box2</div>
-                    <div class="nabox">Box3</div>
-                    <div class="nabox">Box4</div>
-                    <div class="nabox">Box5</div>
+                    <!-- <button onclick="togglePopup()" class="nabox"> -->
+                    <?php
+                        include '../backend/bookinfo.php';
+                        // $products = [
+                        //     ["book_name" => $_SESSION['book_name'], "book_owner" => $_SESSION['book_owner'], "img_src" => $_SESSION['img_src']],
+                        //     ["book_name" => "Book 2", "book_owner" => "Author 2", "img_src" => "image2.jpg"],
+                        //     ["book_name" => "Book 2", "book_owner" => "Author 2", "img_src" => "image2.jpg"],
+                        //     ["book_name" => "Book 2", "book_owner" => "Author 2", "img_src" => "image2.jpg"],
+                        //     ["book_name" => "Book 2", "book_owner" => "Author 2", "img_src" => "image2.jpg"]
+                        // ];
+                        // foreach ($products as $product) {
+                        //     echo '<div class="nabox">';
+                        //     echo $product['book_name'], '<br>';
+                        //     echo $product['book_owner'], '<br>';
+                        //     echo '<img src="' . $product['img_src'] . '" alt="Image">';
+                        //     echo '</div>';
+                        // }
+                    ?>
             </div>
             
         </div>
+                    
+
+        <script src="javascript/addbook.js"></script>
     </body>
 </html>

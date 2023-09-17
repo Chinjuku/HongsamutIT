d56a3aad775f2fed67a16d9f6d3352cb1c3b0594
Your Name <you@example.com>
2023-09-17 16:25:27 +0700

    addbooks

--- frontend/addbook.php
+++ frontend/addbook.php
@@ -8,10 +8,10 @@
 <html lang="en">
 <head>
     <title>HONGSAMUT</title>
-    <link rel="stylesheet" href="css/addbook.css">
+    <link rel="stylesheet" href="./css/addbook.css">
     <link rel="stylesheet" href="plugins/bootstrap/css/bootstrap.min.css">
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.10.5/font/bootstrap-icons.css">
-    <style>
+    <!-- <style>
         body{
             background-color: #FDF5D0;
         }
@@ -19,7 +19,7 @@
             background-color: rgb(255, 255, 255);
             border-radius: 10px;
         }
-    </style>
+    </style> -->
 </head>
 <body>
     <section class="add-books">

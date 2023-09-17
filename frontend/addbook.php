833d8c11abb3252ad03d604e3b7321da16836637
Your Name <you@example.com>
2023-09-17 16:27:33 +0700

    addbooks

--- frontend/addbook.php
+++ frontend/addbook.php
@@ -8,7 +8,9 @@
 <html lang="en">
 <head>
     <title>HONGSAMUT</title>
-    <link rel="stylesheet" href="css/addbook.css">
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="./css/addbook.css">
     <link rel="stylesheet" href="plugins/bootstrap/css/bootstrap.min.css">
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.10.5/font/bootstrap-icons.css">
     <!-- <style>

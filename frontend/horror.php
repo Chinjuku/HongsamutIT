ac816e6ef350f9a19ad82a7c376c6a2975ede8ca
bamibibi <Natthacha9876@gmail.com>
2023-09-19 00:31:26 +0700

    bamibibi

--- frontend/horror.php
+++ frontend/horror.php
@@ -18,7 +18,7 @@
 
             </div>
             <div class="mid">
-                <div class="detective">DETECTIVE</div>
+                <div class="detective">HORROR</div>
                 <div class="container">
                     <?php
                         include '../backend/bookinfo.php';
--- frontend/mystery.php
+++ frontend/mystery.php
@@ -18,7 +18,7 @@
 
             </div>
             <div class="mid">
-                <div class="detective">DETECTIVE</div>
+                <div class="detective">MYSTERY</div>
                 <div class="container">
                     <?php
                         include '../backend/bookinfo.php';

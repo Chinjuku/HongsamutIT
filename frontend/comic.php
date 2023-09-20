d91dcf26bf71322c3e9e563ccb9b635fa0b067c0
bamibibi <Natthacha9876@gmail.com>
2023-09-21 01:01:29 +0700

    bibi

--- frontend/comic.php
+++ frontend/comic.php
@@ -13,7 +13,7 @@
         <link href="https://fonts.googleapis.com/css2?family=Krub:wght@300;400;500;600;700&family=Mitr:wght@200;300;400;500;600;700&display=swap" 
         rel="stylesheet">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
-        <link rel="stylesheet" href="./css/allbook.css">
+        <link rel="stylesheet" href="./css/categories.css">
         <link rel="stylesheet" href="plugins/bootstrap/css/bootstrap.min.css">
         <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.10.5/font/bootstrap-icons.css">
         

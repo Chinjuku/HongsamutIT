fb51766ecdf5ca0d62561764ac6e58558661a69e
ilSPRITEli <gvkgodvilekung@gmail.com>
2023-09-22 00:27:02 +0700

    Update addbook.php

--- backend/addbook.php
+++ backend/addbook.php
@@ -10,7 +10,7 @@
     $stmt = $conn->prepare($sql);
 
     if ($stmt->execute()) {
-        echo '<script>window.location.href = "../frontend/addbook.php";</script>';
+        echo '<script>window.location.href = "../frontend/allbook.php";</script>';
         exit();
     } else {
         echo '<script>window.location.href = "../frontend/addbook.php";</script>';

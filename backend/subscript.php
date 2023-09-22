b42988ca9d95f90540e4c6147ed5abcb99914352
ilSPRITEli <gvkgodvilekung@gmail.com>
2023-09-22 22:32:16 +0700

    t

--- backend/subscript.php
+++ backend/subscript.php
@@ -33,15 +33,33 @@
         $payment_row = $payment_result->fetch_assoc();
         $payment_id = $payment_row['payment_id']; // ดึง payment id จาก payment ที่สร้างไปใหม่
 
+        $subscription_sql = "SELECT * FROM subscription_plans WHERE plan_id = '{$_SESSION['selected_plan_id']}'"; // ดึงข้อมูล subscription plan ที่ user เลือก
+        $subscription_result = $conn->query($subscription_sql); 
+        $subscription_row = $subscription_result->fetch_assoc();  
+        $plan_duration = $subscription_row['duration']; // ดึงระยะเวลา subscription plan ที่ user เลือก
 
-        $token = "wait_for_me dont touch this mf"; // ใส่ token ที่ได้จากการสร้าง token ใน gbprimepay.com
-        $gbprimepay = new GBPrimePay();
-        $qrcode = $gbprimepay->promptpay([ // สร้าง qrcode สำหรับชำระเงิน
-            'amount' => '$plan_price',
-            'referenceNo' => '$payment_id',
-            'backgroundUrl' => 'https://hongsamutit2.iservkmitl.tech/backend/gbprimepay.webhook.php', // ให้ GBPrimePay ส่งข้อมูลกลับมาที่ไหน
-        ], $token);
-        echo '<img src="' . $qrcode . '">'; // แสดง qrcode สำหรับชำระเงิน
+        
+        $update_payment = "UPDATE payments SET is_success = 1 WHERE payment_id = '{$json_obj->referenceNo}'";// อัพเดท payment ว่าสำเร็จ
+        $conn->query($update_payment);
+
+        $current_date = date('Y-m-d');
+        $start_date = $current_date;
+        $end_date = date('Y-m-d', strtotime("+$plan_duration days", strtotime($current_date)));
+
+        $insert_subscription_sql = "INSERT INTO user_subscriptions (user_id, plan_id, date_start, date_end) VALUES ('{$_SESSION['user_id']}', '{$_SESSION['selected_plan_id']}', '{$start_date}', '{$end_date}')"; //
+        $conn->query($insert_subscription_sql);// เพิ่ม subscription plan ให้ user
+        $update_user_sql = "UPDATE user SET plan_id = '{$_SESSION['selected_plan_id']}' WHERE user_id = '{$_SESSION['user_id']}'"; // อัพเดท user ว่ามี subscription plan อะไร
+        $conn->query($update_user_sql);// อัพเดท user ว่ามี subscription plan อะไร
+        echo '<script>alert("success");</script>';
+        echo '<script>window.location.href = "../frontend/profile.php";</script>';
+        // $token = "wait_for_me dont touch this mf"; // ใส่ token ที่ได้จากการสร้าง token ใน gbprimepay.com
+        // $gbprimepay = new GBPrimePay();
+        // $qrcode = $gbprimepay->promptpay([ // สร้าง qrcode สำหรับชำระเงิน
+        //     'amount' => '$plan_price',
+        //     'referenceNo' => '$payment_id',
+        //     'backgroundUrl' => 'https://hongsamutit2.iservkmitl.tech/backend/gbprimepay.webhook.php', // ให้ GBPrimePay ส่งข้อมูลกลับมาที่ไหน
+        // ], $token);
+        // echo '<img src="' . $qrcode . '">'; // แสดง qrcode สำหรับชำระเงิน
         
         
       } else {

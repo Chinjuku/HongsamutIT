44e7a0baeca4db6337b9e03524bd2d206252db52
cheraxm <109953279+cheraxm@users.noreply.github.com>
2023-09-22 22:53:11 +0700

    Update viewhistory

---
+++ frontend/viewhistory.php
@@ -0,0 +1,65 @@
+<?php
+    session_start();
+    include '../backend/database.php';
+    // include './layout/leftbar.php';
+    include './layout/navbar.php';
+?>
+
+<!DOCTYPE html>
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>HONGSAMUT</title>
+        <link rel="stylesheet" href="css/backpack.css">
+        <link rel="preconnect" href="https://fonts.googleapis.com%22%3E/">
+        <link rel="preconnect" href="https://fonts.gstatic.com/" crossorigin>
+        <link href="https://fonts.googleapis.com/css2?family=Krub:wght@300;400;500;600;700&family=Mitr:wght@200;300;400;500;600;700&display=swap" 
+        rel="stylesheet">
+        <link rel="stylesheet" href="plugins/bootstrap/css/bootstrap.min.css">
+        <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.10.5/font/bootstrap-icons.css">
+        <link
+            rel="stylesheet"
+            href="https://fonts.googleapis.com/css2?family=Inter:wght@100;200;300;400;500;600;700;800;900&amp;display=swap"
+            data-tag="font"
+        />
+        <link
+            rel="stylesheet"
+            href="https://fonts.googleapis.com/css2?family=Inter:wght@100;200;300;400;500;600;700;800;900&amp;display=swap"
+            data-tag="font"
+        />
+        <link
+            rel="stylesheet"
+            href="https://fonts.googleapis.com/css2?family=Inter:wght@100;200;300;400;500;600;700;800;900&amp;display=swap"
+            data-tag="font"
+        />
+        <link
+            rel="stylesheet"
+            href="https://fonts.googleapis.com/css2?family=Mitr:wght@200;300;400;500;600;700&amp;display=swap"
+            data-tag="font"
+        />
+        <link
+            rel="stylesheet"
+            href="https://fonts.googleapis.com/css2?family=Mitr:wght@200;300;400;500;600;700&amp;display=swap"
+            data-tag="font"
+        />
+    </head>
+    <body>
+        <div class="main">
+            <div class="mid">
+                <div class="borrow">History of Borrow</div>
+                <table class="table2">
+                    <thead>
+                        <tr>
+                        <th>NAME</th>
+                        <th>TITLE</th>
+                        <th>CATEGORY</th>
+                        <!-- <th>STATUS</th> -->
+                        <th>STARTED</th>
+                        <th>FINISHED</th>
+                        </tr>
+                    </thead>
+                </table>
+            </div>
+
+        </div>
+    </body>

3efd9522bb9162c1b14fbf02a1cd6298e5c8e7f8
bamibibi <Natthacha9876@gmail.com>
2023-09-22 22:53:03 +0700

    bibi

--- backend/bookinfo.php
+++ backend/bookinfo.php
@@ -188,11 +188,11 @@ $conn->close();
 
     /* Style for the popup content */
     .popup-content {
-        background-color: #fff;
-        padding: 20px;
-        /* width: 800px;
-        height: 500px; */
-        border-radius: 5px;
+        background-color: #FDF5D0;
+        padding: 0px;
+        width: 800px;
+        height: 500px;
+        /* border-radius: 5px; */
         box-shadow: 0px 0px 10px 0px rgba(0,0,0,0.5);
     }
 

a1d57c31c02066a937409c7373ca96979bbe793e
Your Name <you@example.com>
2023-09-23 15:47:48 +0700

    allbookez

--- frontend/allbook.php
+++ frontend/allbook.php
@@ -16,12 +16,6 @@
         <link rel="stylesheet" href="css/allbook.css">
         <link rel="stylesheet" href="plugins/bootstrap/css/bootstrap.min.css">
         <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.10.5/font/bootstrap-icons.css">
-        <style>
-            img{
-                width: 150px;
-                height: 180px;
-            }
-        </style>
     </head>
     <body>
 

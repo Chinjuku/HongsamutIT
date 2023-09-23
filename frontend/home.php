c02fc42b807f3cb9ef939807ddbdecb79caf8af7
bamibibi <Natthacha9876@gmail.com>
2023-09-24 00:34:17 +0700

    bibi

--- frontend/home.php
+++ frontend/home.php
@@ -10,7 +10,7 @@
     <head>
         <title>HONGSAMUT</title>
         <meta name="viewport" content="width=1920, height=1080, initial-scale=1">
-        <link rel="stylesheet" href="home.css">
+        <link rel="stylesheet" href="css/home.css">
         <link rel="stylesheet" href="plugins/bootstrap/css/bootstrap.min.css">
         <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.10.5/font/bootstrap-icons.css">
         <style>

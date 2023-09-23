bf0096d1bbedadafbb92a33f3176189370bd322d
ilSPRITEli <95961148+ilSPRITEli@users.noreply.github.com>
2023-09-24 01:45:27 +0700

    esfs

---
+++ backend/test/secrets.php
@@ -0,0 +1,6 @@
+<?php
+// Keep your Stripe API key protected by including it as an environment variable
+// or in a private script that does not publicly expose the source code.
+
+// This is your test secret API key.
+$stripeSecretKey = 'sk_test_51NsgbmKnBwsgVCnlPTl5VDG0tN98RPTfKvTju6EPK1nELqlVprkuB50dLaJ9Kxu0D21K1xUJadxhn8YY8QXsJdY800lYcwhADL';

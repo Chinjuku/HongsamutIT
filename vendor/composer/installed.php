a5518cab715d7b631dc73a0ebc80465eca686aec
ilSPRITEli <95961148+ilSPRITEli@users.noreply.github.com>
2023-09-24 01:48:26 +0700

    g

--- vendor/composer/installed.php
+++ vendor/composer/installed.php
@@ -3,7 +3,7 @@
         'name' => '__root__',
         'pretty_version' => 'dev-main',
         'version' => 'dev-main',
-        'reference' => '7b20b6ec40de9cc31fca79bc747e87ecd274ed51',
+        'reference' => 'bf0096d1bbedadafbb92a33f3176189370bd322d',
         'type' => 'library',
         'install_path' => __DIR__ . '/../../',
         'aliases' => array(),
@@ -13,7 +13,7 @@
         '__root__' => array(
             'pretty_version' => 'dev-main',
             'version' => 'dev-main',
-            'reference' => '7b20b6ec40de9cc31fca79bc747e87ecd274ed51',
+            'reference' => 'bf0096d1bbedadafbb92a33f3176189370bd322d',
             'type' => 'library',
             'install_path' => __DIR__ . '/../../',
             'aliases' => array(),

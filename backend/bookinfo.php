cd70477a2690fbe2a1fc783b67bc421022ccf63d
bamibibi <Natthacha9876@gmail.com>
2023-09-23 17:52:36 +0700

    bibi

--- backend/bookinfo.php
+++ backend/bookinfo.php
@@ -260,7 +260,7 @@ $conn->close();
     }
     .clicktoview:hover{
         background: #657661;
-        box-shadow: 1.5px 1.5px rgb(253,245,208, 0.8);
+        box-shadow: 2px 2px rgb(39,34,34);
     }
     .popup-bookname{
         font-family:"Poppings";

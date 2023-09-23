58c6e3068d62d1511f8ed8a059c8095476570494
ilSPRITEli <95961148+ilSPRITEli@users.noreply.github.com>
2023-09-24 01:34:29 +0700

    f

---
+++ vendor/composer/installed.php
@@ -0,0 +1,32 @@
+<?php return array(
+    'root' => array(
+        'name' => '__root__',
+        'pretty_version' => 'dev-main',
+        'version' => 'dev-main',
+        'reference' => '7b20b6ec40de9cc31fca79bc747e87ecd274ed51',
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => true,
+    ),
+    'versions' => array(
+        '__root__' => array(
+            'pretty_version' => 'dev-main',
+            'version' => 'dev-main',
+            'reference' => '7b20b6ec40de9cc31fca79bc747e87ecd274ed51',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+        'stripe/stripe-php' => array(
+            'pretty_version' => 'v12.4.0',
+            'version' => '12.4.0.0',
+            'reference' => '7d0a90772fc1c179e370971264318208533324b9',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../stripe/stripe-php',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+    ),
+);

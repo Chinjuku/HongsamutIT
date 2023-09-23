4c970661b2951da935e15176001f751b4198ec36
bamibibi <Natthacha9876@gmail.com>
2023-09-24 00:09:02 +0700

    bibi

--- frontend/home.php
+++ frontend/home.php
@@ -9,7 +9,7 @@
     <head>
         <title>HONGSAMUT</title>
         <meta name="viewport" content="width=1920, height=1080, initial-scale=1">
-        <link rel="stylesheet" href="css/allbook.css">
+        <link rel="stylesheet" href="home.css">
         <link rel="stylesheet" href="plugins/bootstrap/css/bootstrap.min.css">
         <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.10.5/font/bootstrap-icons.css">
         <style>
@@ -24,8 +24,8 @@
         <div class="main">
             <div class="mid">
                 <div class="head">
-                    <div class="square"></div>
-                    <div class="newarrival"><b>NEW</b><br>ARRIVALS</div>
+                    <!-- <div class="square"></div> -->
+                    <div class="newarrival">NEW ARRIVALS</div>
                 </div>
                 <div class="container">
                         <!-- ใส่ new book นะ (ใส่ไว้ได้เลย แต่เอาข้อมูลหนังสือใหม่เข้า database ด้วย)-->

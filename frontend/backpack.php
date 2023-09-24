33cea1d24bd9464b3df092eb0a69b7abb47e06e7
Your Name <you@example.com>
2023-09-24 23:30:00 +0700

    users

--- frontend/backpack.php
+++ frontend/backpack.php
@@ -143,11 +143,6 @@
                             }
                           }
                         ?>
-                        <!-- <div class="circle1"></div>
-                        <div class="circle2"></div>
-                        <div class="circle3"></div>
-                        <div class="circle4"></div>
-                        <div class="circle5"></div> -->
                     </div> 
                 </div>
                 

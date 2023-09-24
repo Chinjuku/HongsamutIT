30f94bc0ae9dcc847740d736ba5be25441339418
bamibibi <Natthacha9876@gmail.com>
2023-09-24 22:49:52 +0700

    bamibibi

--- frontend/backpack.php
+++ frontend/backpack.php
@@ -9,7 +9,7 @@
         <meta charset="UTF-8">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
         <title>HONGSAMUT</title>
-        <link rel="stylesheet" href="css/backpack.css">
+        <link rel="stylesheet" href="backpack.css">
         <link rel="preconnect" href="https://fonts.googleapis.com%22%3E/">
         <link rel="preconnect" href="https://fonts.gstatic.com/" crossorigin>
         <link href="https://fonts.googleapis.com/css2?family=Krub:wght@300;400;500;600;700&family=Mitr:wght@200;300;400;500;600;700&display=swap" 

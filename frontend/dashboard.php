286f4a478b6dbc4bf6f59ecb1ccb3ce70525236a
Puay47 <jarupongthongmark@gmail.com>
2023-10-22 02:29:13 +0700

    dashboard

--- frontend/dashboard.php
+++ frontend/dashboard.php
@@ -10,16 +10,18 @@
   <title>HONGSAMUT</title>
   <link rel="stylesheet" href="style.css" />
   <!-- Font Awesome Cdn Link -->
-  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.4/css/all.min.css"/>
+  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.2/css/all.min.css" integrity="sha512-z3gLpd7yknf1YoNbCzqRKc4qyor8gaKU1qmn+CShxbuBusANI9QpRohGBreCFkKxLhei6S9CQXFEbbKuqLg0DA==" crossorigin="anonymous" referrerpolicy="no-referrer" />
   <link rel="stylesheet" href="css/dashboard.css">
 </head>
 <body>
+  
   <div class="container">
+    
 
     <section class="main">
       <div class="main-top">
         <h1>DASHBOARD</h1>
-        <i class="fas fa-user-cog"></i>
+        <i class="fa-solid fa-house"></i>
       </div>
       <div class="users">
         <div class="card">

1c0e845b505028b9cdd1b687b7a8e25efa3054cb
Puay47 <jarupongthongmark@gmail.com>
2023-10-22 02:08:02 +0700

    dashboard

---
+++ frontend/dashboard.php
@@ -0,0 +1,130 @@
+<?php
+    include '../backend/database.php';
+    // include './layout/navbar.php';
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8" />
+  <title>HONGSAMUT</title>
+  <link rel="stylesheet" href="style.css" />
+  <!-- Font Awesome Cdn Link -->
+  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.4/css/all.min.css"/>
+  <link rel="stylesheet" href="css/dashboard.css">
+</head>
+<body>
+  <div class="container">
+
+    <section class="main">
+      <div class="main-top">
+        <h1>DASHBOARD</h1>
+        <i class="fas fa-user-cog"></i>
+      </div>
+      <div class="users">
+        <div class="card">
+          <h4>กราฟ</h4>
+          <p>หนังสือแต่ละประเภท</p>
+        </div>
+        <div class="card">
+          <h4>กราฟ2</h4>
+          <p>หนังสือแต่ละประเภท</p>
+        </div>
+      </div>
+
+      <section class="attendance1">
+        <div class="attendance-list1">
+          <h1>ตาราง1</h1>
+          <table class="table">
+            <thead>
+              <tr>
+                <th>BORROOWID</th>
+                <th>USERNAME</th>
+                <th>BOOKNAME</th>
+                <th>CATEGORY</th>
+                <th>BORROWSTARTEDTIME</th>
+                <th>BORROWENDTIME</th>
+              </tr>
+            </thead>
+            <tbody>
+                      <?php
+                            include '../backend/database.php';
+                            
+                            $sql = "SELECT * FROM borrow_books";
+                            $result = $conn->query($sql);
+
+                            if ($result->num_rows > 0) {
+                                while($row = $result->fetch_assoc()) {
+                                    $sql2 = "SELECT * FROM users WHERE user_id = '{$row['user_id']}'";
+                                    $result2 = $conn->query($sql2);
+                                    while($row2 = $result2->fetch_assoc()) {
+                                        $username = $row2['user_name'];
+                                    }
+                                    $sql3 = "SELECT * FROM books WHERE book_id = '{$row['book_id']}'";
+                                    $result3 = $conn->query($sql3);
+                                    while($row3 = $result3->fetch_assoc()) {
+                                        $bookname = $row3['book_name'];
+                                        $sql4 = "SELECT * FROM categories WHERE cate_id = '{$row3['cate_id']}'";
+                                        $result4 = $conn->query($sql4);
+                                        while($row4 = $result4->fetch_assoc()) {
+                                        $category = $row4['category_name'];
+                                        }
+                                    }
+                                    echo "<tr>";
+                                    echo "<td>" . $row['borrow_id'] . "</td>";
+                                    echo "<td>" . $username . "</td>";
+                                    echo "<td>" . $bookname . "</td>";
+                                    echo "<td>" . $category . "</td>";
+                                    echo "<td>" . $row['date_borrow'] . "</td>";
+                                    echo "<td>" . $row['date_return'] . "</td>";
+                                    echo "</tr>";
+                                }
+                            }
+                        ?>
+                    </tbody>
+          </table>
+        </div>
+    </section>
+    <section class="attendance2">
+        <div class="attendance-list2">
+          <h1>ตาราง</h1>
+          <table class="table">
+            <thead>
+              <tr>
+                <th>ID</th>
+                <th>USERNAME</th>
+                <th>PAID</th>
+                <th>DATE</th>
+                
+              </tr>
+            </thead>
+            <tbody>
+                    <?php
+                        include '../backend/database.php';
+                        
+                        $sql = "SELECT * FROM payments";
+                        $result = $conn->query($sql);
+
+                        if ($result->num_rows > 0) {
+                            while($row = $result->fetch_assoc()) {
+                                $sql2 = "SELECT * FROM users where user_id = '{$row['user_id']}'";
+                                $results = $conn->query($sql2);
+                                while($rows = $results->fetch_assoc()) {
+                                $username = $rows['user_name'];
+                            }
+                                echo "<tr>";
+                                echo "<td>" . $row['payment_id'] . "</td>";
+                                echo "<td>" . $username . "</td>";
+                                echo "<td>" . $row['amount'] . "</td>";
+                                echo "<td>" . $row['date_paid'] . "</td>";
+                                echo "</tr>";
+                            }
+                        }
+                    ?>
+            </tbody>
+          </table>
+        </div>
+      </section> 
+  </div>
+</body>
+</html>
--- frontend/historypayment.php
+++ frontend/historypayment.php
@@ -1,87 +1,70 @@
 <?php
-    include '../backend/database.php';
-    // include './layout/leftbar.php';
-    include './layout/navbar.php';
+include '../backend/database.php';
+// include './layout/leftbar.php';
+include './layout/navbar.php';
 ?>
 
 <!DOCTYPE html>
-    <head>
-        <meta charset="UTF-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1.0">
-        <title>HONGSAMUT</title>
-        <link rel="stylesheet" href="css/historypage.css">
-        <link rel="preconnect" href="https://fonts.googleapis.com%22%3E/">
-        <link rel="preconnect" href="https://fonts.gstatic.com/" crossorigin>
-        <link href="https://fonts.googleapis.com/css2?family=Krub:wght@300;400;500;600;700&family=Mitr:wght@200;300;400;500;600;700&display=swap" 
-        rel="stylesheet">
-        <link rel="stylesheet" href="plugins/bootstrap/css/bootstrap.min.css">
-        <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.10.5/font/bootstrap-icons.css">
-        <link
-            rel="stylesheet"
-            href="https://fonts.googleapis.com/css2?family=Inter:wght@100;200;300;400;500;600;700;800;900&amp;display=swap"
-            data-tag="font"
-        />
-        <link
-            rel="stylesheet"
-            href="https://fonts.googleapis.com/css2?family=Inter:wght@100;200;300;400;500;600;700;800;900&amp;display=swap"
-            data-tag="font"
-        />
-        <link
-            rel="stylesheet"
-            href="https://fonts.googleapis.com/css2?family=Inter:wght@100;200;300;400;500;600;700;800;900&amp;display=swap"
-            data-tag="font"
-        />
-        <link
-            rel="stylesheet"
-            href="https://fonts.googleapis.com/css2?family=Mitr:wght@200;300;400;500;600;700&amp;display=swap"
-            data-tag="font"
-        />
-        <link
-            rel="stylesheet"
-            href="https://fonts.googleapis.com/css2?family=Mitr:wght@200;300;400;500;600;700&amp;display=swap"
-            data-tag="font"
-        />
-    </head>
-    <body>
-        <div class="main">
-            <div class="mid">
-                <?php
-                    include './layout/historynav.php';
-                ?>
-                <div class="payment"><h2 style="font-size: 50px;text-align: center;">History of Payment</h2></div>
-                <table class="table2">
-                    <thead>
-                        <tr>
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>HONGSAMUT</title>
+    <link rel="stylesheet" href="css/historypage.css">
+    <link rel="preconnect" href="https://fonts.googleapis.com%22%3E/">
+    <link rel="preconnect" href="https://fonts.gstatic.com/" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Krub:wght@300;400;500;600;700&family=Mitr:wght@200;300;400;500;600;700&display=swap" rel="stylesheet">
+    <link rel="stylesheet" href="plugins/bootstrap/css/bootstrap.min.css">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.10.5/font/bootstrap-icons.css">
+    <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Inter:wght@100;200;300;400;500;600;700;800;900&amp;display=swap" data-tag="font" />
+    <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Inter:wght@100;200;300;400;500;600;700;800;900&amp;display=swap" data-tag="font" />
+    <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Inter:wght@100;200;300;400;500;600;700;800;900&amp;display=swap" data-tag="font" />
+    <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Mitr:wght@200;300;400;500;600;700&amp;display=swap" data-tag="font" />
+    <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Mitr:wght@200;300;400;500;600;700&amp;display=swap" data-tag="font" />
+</head>
+
+<body>
+    <div class="main">
+        <div class="mid">
+            <?php
+            include './layout/historynav.php';
+            ?>
+            <div class="payment">
+                <h2 style="font-size: 50px;text-align: center;">History of Payment</h2>
+            </div>
+            <table class="table2">
+                <thead>
+                    <tr>
                         <th>USERNAME</th>
                         <th>PAID</th>
                         <th>DATE</th>
-                        </tr>
-                    </thead>
-                    <tbody>
-                        <?php
-                            include '../backend/database.php';
-                            
-                            $sql = "SELECT * FROM payments";
-                            $result = $conn->query($sql);
+                    </tr>
+                </thead>
+                <tbody>
+                    <?php
+                    include '../backend/database.php';
 
-                            if ($result->num_rows > 0) {
-                                while($row = $result->fetch_assoc()) {
-                                    $sql2 = "SELECT * FROM users where user_id = '{$row['user_id']}'";
-                                    $results = $conn->query($sql2);
-                                    while($rows = $results->fetch_assoc()) {
-                                        $username = $rows['user_name'];
-                                    }
-                                    echo "<tr>";
-                                    echo "<td>" . $username . "</td>";
-                                    echo "<td>" . $row['amount'] . "</td>";
-                                    echo "<td>" . $row['date_paid'] . "</td>";
-                                    echo "</tr>";
-                                }
+                    $sql = "SELECT * FROM payments";
+                    $result = $conn->query($sql);
+
+                    if ($result->num_rows > 0) {
+                        while ($row = $result->fetch_assoc()) {
+                            $sql2 = "SELECT * FROM users where user_id = '{$row['user_id']}'";
+                            $results = $conn->query($sql2);
+                            while ($rows = $results->fetch_assoc()) {
+                                $username = $rows['user_name'];
                             }
-                        ?>
-                    </tbody>
-                </table>
-            </div>
-
+                            echo "<tr>";
+                            echo "<td>" . $username . "</td>";
+                            echo "<td>" . $row['amount'] . "</td>";
+                            echo "<td>" . $row['date_paid'] . "</td>";
+                            echo "</tr>";
+                        }
+                    }
+                    ?>
+                </tbody>
+            </table>
         </div>
-    </body>
+
+    </div>
+</body>

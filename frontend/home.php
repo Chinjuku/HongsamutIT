e6a853c60e2b9b971eb99a3d09f9c80fd4118d16
bamibibi <109953620+bamibibi@users.noreply.github.com>
2023-10-21 21:20:40 +0700

    home/viewbook ja

--- frontend/home.php
+++ frontend/home.php
@@ -1,10 +1,8 @@
 <?php
     // session_start();
     include './layout/navbar.php';
-    include './layout/sidebar.php';
+    // include './layout/sidebar.php';
     include '../backend/database.php';
-    include '../backend/update_db.php';
-    update_amount_book($conn);
     
 ?>
 
@@ -12,7 +10,7 @@
     <head>
         <title>HONGSAMUT</title>
         <meta name="viewport" content="width=1920, height=1080, initial-scale=1">
-        <link rel="stylesheet" href="css/home.css">
+        <link rel="stylesheet" href="home.css">
         <link rel="stylesheet" href="plugins/bootstrap/css/bootstrap.min.css">
         <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.10.5/font/bootstrap-icons.css">
         <style>
@@ -25,11 +23,23 @@
     <body>
 
         <div class="main">
-            <div class="mid">
-                <div class="greenbg"></div>
-                <div class="head">
-                    <div class="newarrival"><i>NEW ARRIVALS</i></div>
+            <div class="greenbg">
+                <div class="greentext">
+                    <div class="gt1">Welcome to our <br>E-Book library</div>
+                    <div class="gt2">you can explore all books in our library for free.</div>
+                    <img class="img1" src="../img/homecover.jpg" alt="Girl in a jacket" style="width:250px;height:350px;">
+                    <img class="img3" src="../img/img3.png" alt="Girl in a jacket" style="width:250px;height:350px;">
+                    <img class="img2" src="../img/img2.jpg" alt="Girl in a jacket" style="width:250px;height:350px;">
+                    
                 </div>
+                <button class="explorebut">EXPLORE OUR BOOKS</button>
+            </div>
+            <div class="mid">
+                    <!-- <div class="square"></div> -->
+                    <div class="newarrival"><i>NEW ARRIVALS</i></div>
+                    <hr>
+                    <div class="newarrdes">our newest books is here!</div>
+
                 <div class="container">
                     <!-- ใส่ new book นะ (ใส่ไว้ได้เลย แต่เอาข้อมูลหนังสือใหม่เข้า database ด้วย)-->
                     <?php
@@ -37,7 +47,7 @@
                         $result = $conn->query($sql);
                         $num = 1;
                         while ($row = $result->fetch_assoc()){
-                            if($num <= 8){
+                            if($num <= 5){
                                 echo '<div class="nabox">';
                                 echo '<img class="pic" src="' . $row['imgsrc'] . '" alt="Image">', '<br>';
                                 echo '<p class="bookname">' . $row['book_name'] . '</p>';
@@ -55,7 +65,26 @@
                 <div class="head2">
                     <div class="recommend"><i>RECOMMENDED BOOKS</i></div>
                 </div>
-                <div class="container">
+                <div class="container"> 
+                <?php
+                        $sql = "SELECT * from books order by book_id desc";
+                        $result = $conn->query($sql);
+                        $num = 1;
+                        while ($row = $result->fetch_assoc()){
+                            if($num <= 5){
+                                echo '<div class="nabox">';
+                                echo '<img class="pic" src="' . $row['imgsrc'] . '" alt="Image">', '<br>';
+                                echo '<p class="bookname">' . $row['book_name'] . '</p>';
+                                echo '<p>' . $row['book_owner'] . '</p>';
+                                // echo '<button class="clicktoview" onclick="togglePopup(\'' . $row['book_name'] . '\', \'' . $row['book_owner'] . '\', \'' . $row['imgsrc'] . '\',  \'' . $row['book_id'] . '\')">VIEW</button>';
+                                echo '</div>';
+                                $num++;
+                                }
+                            else{
+                                break;
+                            }
+                        }
+                    ?>
         
             </div>
         </div>
---
+++ frontend/newarrivals.php
@@ -0,0 +1,59 @@
+@@ -0,0 +1,58 @@
+<?php
+    // session_start();
+    include './layout/navbar.php';
+    include './layout/sidebar.php';
+    include '../backend/database.php';
+    
+?>
+
+<!DOCTYPE html>
+    <head>
+        <title>HONGSAMUT</title>
+        <meta name="viewport" content="width=1920, height=1080, initial-scale=1">
+        <link rel="stylesheet" href="css/newarrivals.css">
+        <link rel="stylesheet" href="plugins/bootstrap/css/bootstrap.min.css">
+        <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.10.5/font/bootstrap-icons.css">
+        <style>
+            img{
+                width: 150px;
+                height: 180px;
+            }
+        </style>
+    </head>
+    <body>
+
+        <div class="main">
+            <div class="mid">
+                <div class="greenbg"></div>
+                <div class="head">
+                    <div class="newarrival"><i>NEW ARRIVALS</i></div>
+                </div>
+                <div class="container">
+                    <!-- ใส่ new book นะ (ใส่ไว้ได้เลย แต่เอาข้อมูลหนังสือใหม่เข้า database ด้วย)-->
+                    <?php
+                        $sql = "SELECT * from books order by book_id desc";
+                        $result = $conn->query($sql);
+                        $num = 1;
+                        while ($row = $result->fetch_assoc()){
+                            if($num <= 8){
+                                echo '<div class="nabox">';
+                                echo '<img class="pic" src="' . $row['imgsrc'] . '" alt="Image">', '<br>';
+                                echo '<p class="bookname">' . $row['book_name'] . '</p>';
+                                echo '<p>' . $row['book_owner'] . '</p>';
+                                // echo '<button class="clicktoview" onclick="togglePopup(\'' . $row['book_name'] . '\', \'' . $row['book_owner'] . '\', \'' . $row['imgsrc'] . '\',  \'' . $row['book_id'] . '\')">VIEW</button>';
+                                echo '</div>';
+                                $num++;
+                                }
+                            else{
+                                break;
+                            }
+                        }
+                    ?>
+                </div>
+
+        
+            </div>
+        </div>
+    </body>
+</html>

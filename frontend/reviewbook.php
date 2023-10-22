41401b36d7d5cae6a10d9502beb640a258a045e8
bamibibi <109953620+bamibibi@users.noreply.github.com>
2023-10-22 21:11:40 +0700

    reviewbook2

--- frontend/reviewbook.php
+++ frontend/reviewbook.php
@@ -10,7 +10,7 @@
     <title>HONGSAMUT</title>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="css/reviewbook2.css">
+    <link rel="stylesheet" href="css/reviewbook.css">
     <link rel="stylesheet" href="plugins/bootstrap/css/bootstrap.min.css">
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.10.5/font/bootstrap-icons.css">
 </head>

2365541f8279d7d5754c03c92f2804c42eadc9c8
Chinjung <23048act@gmail.com>
2023-10-22 20:27:10 +0700

    grsseghegre

--- backend/recommended.php
+++ backend/recommended.php
@@ -1,26 +1,20 @@
 <?php
     include 'database.php';
     session_start();
-    $userid = $_SESSION['user_id'];
-    $sql2 = "SELECT user_id, MAX(cate_id) AS maxCate, COUNT(cate_id) AS countCate
-            FROM borrow_books
-            WHERE user_id = ?
-            GROUP BY cate_id;";
-    $stmt = $conn->prepare($sql2);
-    $stmt->bind_param("i", $userid);
+    // $userid = $_SESSION["user_id"];
+    // $sql2 = "SELECT user_id, MAX(cate_id) AS maxCate, COUNT(cate_id) AS countCate
+    //         FROM borrow_books
+    //         WHERE user_id = ?
+    //         GROUP BY user_id;";
+    // $stmt2 = $conn->prepare($sql2);
+    // $stmt2->bind_param("i", $userid);
+    // $stmt2->execute();
+    // $result2 = $stmt2->get_result();
+    // $cate_id = null;
 
-    // Execute the query
-    $stmt->execute();
-
-    // Get the result
-    $result2 = $stmt->get_result();
-
-    // Initialize $cate_id
-    $cate_id = null;
-
-    while ($row2 = $result2->fetch_assoc()) {
-        $_SESSION['max_cate'] = $row2['maxCate'];
-        $count = $row2['countCate'];
-    }
-    echo "max" , $_SESSION['max_cate'] , "count" , $count;
-?>
+    // while ($row2 = $result2->fetch_assoc()) {
+    //     $_SESSION['max_cate'] = $row2['maxCate'];
+    //     $count = $row2['countCate'];
+    // }
+    echo "max" , $_SESSION['max_cate'] ," ", "count" , $count;
+// ?>

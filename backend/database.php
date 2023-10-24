2810f13ffbfecc9413f55bb0ce53e35d86a7036e
Chinjung <23048act@gmail.com>
2023-10-24 14:49:40 +0700

    cd

--- backend/database.php
+++ backend/database.php
@@ -1,11 +1,11 @@
 <?php
-    // $host = "localhost";
-    // $dbUsername = "root";
-    $host = "161.246.127.24";
-    $dbUsername = "admin";
+    $host = "localhost";
+    $dbUsername = "root";
+    // $host = "161.246.127.24";
+    // $dbUsername = "admin";
 
-    // $conn = new mysqli($host, $dbUsername, "", "ebook", 3306);
-    $conn = new mysqli($host, $dbUsername, "admin", "hongsamutit", 9059);
+    $conn = new mysqli($host, $dbUsername, "", "elibrary", 3306);
+    // $conn = new mysqli($host, $dbUsername, "admin", "hongsamutit", 9059);
 
     if($conn->connect_error) {
         die("connection failed: " . $conn->connect_error);
--- backend/review.php
+++ backend/review.php
@@ -5,24 +5,75 @@
     date_default_timezone_set('Asia/Bangkok');
     $datereview = date('Y-m-d H:i:s');
     $bookid = $_POST['book_id'];
+    $userId = $_SESSION['user_id'];
     $_SESSION['book_id'] = $bookid;
-
     $comment = $_POST['comment'];
 
-    $sql = "INSERT INTO review (user_id, book_id, review_in, datetime_review, like_amount) 
-            VALUES (?, ?, ?, ?, 0)";
-    $stmt = $conn->prepare($sql);
+    class ReviewController {
+        private $conn;
 
-    $stmt->bind_param("iiss", $_SESSION['user_id'], $bookid, $comment, $datereview);
+        public function __construct($conn) {
+            $this->conn = $conn;
+        }
 
-    if ($stmt->execute()) {
-        // echo "<script>alert('Review submitted!: " .$comment. "');</script>";
-        echo "<script>window.location.href='../frontend/reviewbook.php';</script>";    
+        public function submitReview($bookid, $comment, $datereview, $userId) {
+            $sql = "INSERT INTO review (user_id, book_id, review_in, datetime_review, like_amount) VALUES (?, ?, ?, ?, 0)";
+            $stmt = $this->conn->prepare($sql);
+
+            $stmt->bind_param("iiss", $userId, $bookid, $comment, $datereview);
+
+            if ($stmt->execute()) {
+                // Review successfully submitted
+                return true;
+            } else {
+                // Review submission failed
+                return false;
+            }
+        }
+    }
+
+    $reviewController = new ReviewController($conn);
+    $userId = $_SESSION['user_id'];
+
+    if ($reviewController->submitReview($bookid, $comment, $datereview, $userId)) {
+        echo "<script>window.location.href='../frontend/reviewbook.php';</script>";
         exit();
     } else {
-        echo "Error: " . $stmt->error;
+        echo "Error: " . $conn->error;
         echo "<script>alert('Review doesn't sent!');</script>";
         echo '<script>window.location.href = "../frontend/reviewbook.php";</script>';
         exit();
     }
+?>
+
+<?php
+    // include 'database.php';
+    // session_start();
+
+    // date_default_timezone_set('Asia/Bangkok');
+    // $datereview = date('Y-m-d H:i:s');
+    // $bookid = $_POST['book_id'];
+    // $_SESSION['book_id'] = $bookid;
+    // $comment = $_POST['comment'];
+
+    // function review($conn, $bookid, $comment, $datereview){
+    //     $sql = "INSERT INTO review (user_id, book_id, review_in, datetime_review, like_amount) 
+    //             VALUES (?, ?, ?, ?, 0)";
+    //     $stmt = $conn->prepare($sql);
+
+    //     $stmt->bind_param("iiss", $_SESSION['user_id'], $bookid, $comment, $datereview);
+
+    //     if ($stmt->execute()) {
+    //         // echo "<script>alert('Review submitted!: " .$comment. "');</script>";
+    //         echo "<script>window.location.href='../frontend/reviewbook.php';</script>";    
+    //         exit();
+    //     } else {
+    //         echo "Error: " . $stmt->error;
+    //         echo "<script>alert('Review doesn't sent!');</script>";
+    //         echo '<script>window.location.href = "../frontend/reviewbook.php";</script>';
+    //         exit();
+    //     }
+    // }
+
+    // review($conn, $bookid, $comment, $datereview);
 ?>
